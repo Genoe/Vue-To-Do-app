6673589d3c25874eeb9c701ae8a736d18d6e31ca
Wyatt Weisensel <wweisensel@gmail.com>
2020-05-21 19:53:58 -0500

    Added animation

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-
 var testData = [
     { Text: "Lorem ipsum dolor sit amet", IsDone: false },
     { Text: "Sed fringilla tellus nec finibus maximus. Duis venenatis risus purus. Duis magna odio, eleifend vitae dignissim gravida, semper volutpat nisl", IsDone: false },
     { Text: "Ut nec erat non nulla ultricies condimentum", IsDone: true },
 ];
 
+var itemIndex = 0;
+
+function GetFreeItemIndex() {
+    itemIndex++;
+    return itemIndex;
+}
+
 var todoListApp = new Vue({
     el: '#app-todolist',
     data: {
-      todoItems: testData,
-      newTodoText: ""
+        todoItems: testData,
+        newTodoText: ""
     },
     methods: {
 
         addTodo: function() {
-      
-          if (this.newTodoText.trim() == "")
-            return;
-      
-          this.todoItems.push({ Text: this.newTodoText, IsDone: false});
-          this.newTodoText = "";
-          
+
+            if (this.newTodoText.trim() === "")
+                return;
+
+            this.todoItems.push({ Id: GetFreeItemIndex(), Text: this.newTodoText, IsDone: false});
+            this.newTodoText = "";
+
         },
 
         removeTodo: function(todo) {
             this.todoItems.splice(this.todoItems.indexOf(todo), 1);
-          }
-      }
+        }
+    }
 });
